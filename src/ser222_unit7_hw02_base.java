0e2de82e22b952dd106494539e8461bbcab1986b
jebadirad <jebadira@asu.edu>
2018-07-24 01:12:14 -0700

    init

---
+++ src/ser222_unit7_hw02_base.java
@@ -0,0 +1,292 @@
+/**
+ * Write a description of class Base_HW01 here.
+ * 
+ * @author (your name), Acuna
+ * @version (a version number or a date)
+ */
+import java.util.Random;
+
+public class ser222_unit7_hw02_base
+{
+    //standard console size in characters.
+    private static final int LEVEL_HEIGHT = 25;
+    private static final int LEVEL_WIDTH = 80;       
+    
+    private static final char ICON_WALL = '#';           
+    private static final char ICON_BLANK = ' ';
+
+    /**
+     * Returns a 2D array containing a statically created maze with dimentions 80x24.
+     * 
+     * @return     2D array containing a maze 
+     */
+    private static char[][] makeMazeStatic()
+    {
+        //the following maze was generated with the recursive division method and then modified by hand.
+        
+        char level[][] =     
+        {{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}, 
+        {'#', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#', '#', ' ', '#', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', ' ', ' ', '#', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#', ' ', '#', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', '#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', ' ', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', '#', ' ', '#', '#', ' ', '#', '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', '#', '#', ' ', '#', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#'}, 
+        {'#', ' ', '#', ' ', '#', '#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}, 
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'}, 
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},         
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}};
+    
+        return level;
+    }
+    
+    /**
+     * Creates a random maze in a 2D array.
+     * 
+     * @see <a href="http://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_division_method">Recursive_division_method</a>
+     * @return 2D array containing a maze
+     */
+    private static char[][] makeMaze()
+    {
+        char level[][] = createBlankLevel();
+        
+        makeMazeRecursive(level, 1, 1, LEVEL_WIDTH-2, LEVEL_HEIGHT-2); //TODO: may need to change but probably not.
+        
+        return level;
+    }
+
+    /**
+     * Creates an empty level of standard level height and width. Level will be
+     * blank but bordered with wall characters.
+     * 
+     * @return  2D array containing a maze
+     */
+    private static char[][] createBlankLevel()
+    {   
+        char level[][] = new char[LEVEL_HEIGHT][LEVEL_WIDTH];
+        
+        //reset level to be entirely blank
+        for (int y = 0; y < LEVEL_HEIGHT; y++) 
+            for (int x = 0; x < LEVEL_WIDTH; x++)
+                level[y][x] = ' ';
+    
+        //top barrier   
+        for (int x = 0; x < LEVEL_WIDTH; x++)
+            level[0][x] = ICON_WALL;
+    
+        //bottom barrier
+        for (int x = 0; x < LEVEL_WIDTH; x++)
+            level[LEVEL_HEIGHT-1][x] = ICON_WALL;
+    
+        //left barrier
+        for (int y = 0; y < LEVEL_HEIGHT; y++)
+            level[y][0] = ICON_WALL;
+    
+        //left barrier
+        for (int y = 0; y < LEVEL_HEIGHT; y++)
+            level[y][LEVEL_WIDTH-1] = ICON_WALL;
+            
+        return level;
+    }
+
+    //TODO: complete method.
+    private static void makeMazeRecursive(char[][]level, int startX, int startY, int endX, int endY)
+    {
+        //column is x
+        //row is Y
+        //calculate area
+        if(validSubArea(startX, endX, startY,endY)){
+            //y is row
+            //x is column
+            int randomCol = randBetween(startX, endX);
+            while(randomCol - startX <= 1 || endX - randomCol <= 1){
+                if(randomCol - startX <= 1 && endX - randomCol <= 1){
+                    randomCol = endX;
+                    break;
+                }else{
+                    randomCol = randBetween(startX, endX);
+
+                }
+            }
+
+
+
+            int randomRow = randBetween(startY, endY);
+            while(randomRow - startY <= 1 || endY - randomRow <= 1){
+                if(randomRow - startY <= 1 && endY - randomRow <= 1){
+                    randomRow = endY;
+                }else{
+                    randomRow = randBetween(startY,endY);
+
+                }
+            }
+
+
+
+            for(int i = startY; i <= endY; i++){
+                //iterate rows
+                //fix column, set rows
+
+                    level[i][randomCol] = '#';
+
+
+                for(int j = startX; j <= endX; j++){
+                    //iterate cols
+                    //fix row, set cols.
+
+                        level[randomRow][j] = '#';
+
+
+                }
+            }
+            int leftOpen = randBetween(startX + 1, randomCol - 1);
+            /*while(leftOpen == randomCol || leftOpen == startX){
+                leftOpen = randBetween(startX + 1, randomCol - 1);
+            }*/
+
+            int rightOpen = randBetween(randomCol + 1, endX - 1);
+            /*while(rightOpen == randomCol || rightOpen == endX){
+                rightOpen = randBetween(randomCol + 1, endX - 1);
+            }*/
+            int upOpen = randBetween(startY + 1, randomRow - 1);
+            /*while(upOpen == startY || upOpen == randomRow){
+                upOpen = randBetween(startY + 1 , randomRow - 1);
+            }*/
+
+            int downOpen = randBetween(randomRow + 1 , endY - 1);
+            /*while(downOpen == randomRow || downOpen == endY){
+                downOpen = randBetween(randomRow + 1, endY - 1);
+            }*/
+            int wallNotOpen = randBetween(1,4);
+            switch(wallNotOpen){
+                case 1:
+                     //left closed
+                    level[randomRow][rightOpen] = ' ';
+                    level[upOpen][randomCol] = ' ';
+                    level[downOpen][randomCol] = ' ';
+                    break;
+                case 2:
+                    //right closed
+                    level[randomRow][leftOpen] = ' ';
+                    level[upOpen][randomCol] = ' ';
+                    level[downOpen][randomCol] = ' ';
+                    break;
+                case 3 :
+                    level[randomRow][leftOpen] = ' ';
+                    level[randomRow][rightOpen] = ' ';
+                    level[downOpen][randomCol] = ' ';
+                    //up closed
+                    break;
+                case 4 :
+                    level[randomRow][leftOpen] = ' ';
+                    level[randomRow][rightOpen] = ' ';
+                    level[upOpen][randomCol] = ' ';
+                    //down closed
+                    break;
+                default:
+                    level[randomRow][leftOpen] = ' ';
+                    level[randomRow][rightOpen] = ' ';
+                    level[upOpen][randomCol] = ' ';
+                    //down closed.
+                    break;
+            }
+
+            //top left
+            makeMazeRecursive(level, startX, startY, randomCol , randomRow );
+            //bottom right
+            makeMazeRecursive(level, randomCol, randomRow, endX, endY);
+            //bottom left
+            makeMazeRecursive(level, startX, randomRow,randomCol,endY);
+            //top right
+            makeMazeRecursive(level, randomCol, startY, endX, randomRow);
+        }
+    }
+
+    /**
+     * area must be greater than 3x3, and ensure that not 1 side has a width of less
+     * than 1.
+     * @param lx
+     * @param ux
+     * @param ly
+     * @param uy
+     * @return
+     */
+    public static boolean validSubArea(int lx, int ux, int ly, int uy){
+        boolean valid = true;
+        int xLength = ux - lx;
+        int yLength = uy - ly;
+
+        if(xLength <= 3){
+            valid = false;
+        }
+        if(yLength <= 3){
+            valid = false;
+        }
+        return valid;
+
+    }
+    /**
+     * Returns a random number inside the interval including the endpoints;
+     * @param l the lower bound
+     * @param u the upper bound
+     * @return random generated int between the bounds
+     */
+    public static int randBetween(int l, int u){
+        int lowerBound = l;
+        int upperBound = u;
+        if(l > u){
+            lowerBound = u;
+            upperBound = l;
+        }
+
+            Random rand = new Random();
+            int bound = (upperBound) - (lowerBound);
+
+            int randomNum = rand.nextInt(bound + 1) + (lowerBound);
+            
+        return randomNum;
+    }
+    
+    /**
+     * Displays a level in the console.
+     * 
+     * @param level  2D array containing a maze
+     */
+    private static void drawLevel(char[][] level)
+    {
+        int y, x;
+    
+        for (y = 0; y < LEVEL_HEIGHT; y++) 
+        {
+            for (x = 0; x < LEVEL_WIDTH; x++)
+                System.out.print(level[y][x]);
+            System.out.println();
+        }
+    }
+
+    /**
+     * Entry point.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {       
+        //show static maze (uncomment for sample output)
+        //drawLevel(makeMazeStatic());
+        //show recursive maze
+        drawLevel(makeMaze());
+    }
+}
